dbe883e1de3556b4909fd4e35ef1b4f3e9b99d9d
Kesse Jones <kjonesfc@outlook.com>
2021-01-14 19:41:22 -0300

    Removing unused code

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,5 @@
 import packageJson from './package.json';
 import typescript from "rollup-plugin-typescript2";
-// import { nodeResolve } from '@rollup/plugin-node-resolve';
-// import commonjs from '@rollup/plugin-commonjs';
 
 const external = [
     ...Object.keys(packageJson.dependencies || {}),
@@ -20,10 +18,6 @@ export default {
         }
     ],
     plugins: [
-        // nodeResolve(),
-        // commonjs({
-        //     include: /node_modules/,
-        // }),
         typescript(),
     ],
     external,
